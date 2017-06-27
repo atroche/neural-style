5e9afe1cc0c36390e1132e7d052f6b1ba2cdd528
Alistair Roche <roche.a@gmail.com>
2017-06-27 10:39:29 +1000

    Add package metadata

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-gcloud ml-engine jobs submit training neural_style_test_1 \
---job-dir gs://wombatflow/jobs/neural_style_test_1 \
+gcloud ml-engine jobs submit training neural_style_test_6 \
+--job-dir gs://wombatflow/jobs/neural_style_test_6 \
 --runtime-version 1.2 \
 --module-name neural_style.trainer.neural_style \
---package-path ./ \
+--package-path trainer/ \
+--config config.yml \
 --region $REGION \
 -- \
 --content gs://wombatflow/data/examples/1-content.jpg \
@@ -14,6 +15,18 @@ gcloud ml-engine jobs submit training neural_style_test_1 \
 gcloud ml-engine local train \
 --module-name neural_style.trainer.neural_style \
 --package-path ./ \
+-- \
+--content gs://wombatflow/data/examples/1-content.jpg \
+--styles gs://wombatflow/data/examples/1-style.jpg \
+--output gs://wombatflow/data/output/output.jpg
+
+
+gcloud ml-engine jobs submit training neural_style_test_3 \
+--job-dir gs://wombatflow/jobs/neural_style_test_3 \
+--runtime-version 1.2 \
+--module-name trainer.neural_style \
+--package-path trainer/ \
+--region $REGION \
 -- \
 --content gs://wombatflow/data/examples/1-content.jpg \
 --styles gs://wombatflow/data/examples/1-style.jpg \
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='neural_style',
+      version='0.0.1',
+      description='Hrm',
+      url='http://github.com/atroche/neural_style',
+      author='Flying Circus',
+      author_email='roche.a@gmail.com',
+      license='MIT',
+      packages=['trainer'],
+      install_requires=['numpy', 'Pillow', 'scipy'],
+      zip_safe=False)
